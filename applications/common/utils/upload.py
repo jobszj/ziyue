520741a07a8c2781ed2a8f59f4ad3ebea1a7d4bd
ziran <gfyftyft@www.com>
2025-03-19 18:18:14 +0800

    1、优化了目录结构，并添加了调用算法的结构，优化了上传

--- applications/common/utils/upload.py
+++ applications/common/utils/upload.py
@@ -18,7 +18,6 @@ def get_photo(page, limit):
 def upload_one(photo, mime):
     filename = photos.save(photo)
     file_url = '/static/upload/'+filename
-    # file_url = photos.url(filename)
     upload_url = current_app.config.get("UPLOADED_PHOTOS_DEST")
     size = os.path.getsize(upload_url + '/' + filename)
     photo = Photo(name=filename, href=file_url, mime=mime, size=size)
